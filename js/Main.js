322c25885876f23f13dc1c32c7f5e3c34ed29a73
axiotroph <izz@izabyl.com>
2018-11-28 06:31:11 -0800

    multiple players

--- js/Main.js
+++ js/Main.js
@@ -9,11 +9,16 @@ class Zealantica {
     this.battle = new Battle();
     this.ui = new UI();
 
+    this.players = {
+      0: this.ui,
+      1: this.ui
+    }
+
     this.ui.load(this.battle).then(this.doBattle.bind(this)).catch(err => {log.fatal("uncaught error"); console.dir(err);});
   }
 
   doBattle(){
-    return this.ui.getTurn().then(turn => {
+    return this.players[this.battle.activePlayer].getTurn().then(turn => {
       this.battle.applyAction(turn);
       this.ui.update();
     }).then(this.doBattle.bind(this));
